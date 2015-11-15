7fab9ebda27defd9f0e34d90acd9e8deebe3e35a
ace <343656921@qq.com>
2015-11-15 17:31:23 +0800

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,4 @@
 $(function() {
-    Date.prototype.format = function(fmt) {
-        var o = {
-            'M+': this.getMonth() + 1, //月份 
-            'd+': this.getDate(), //日 
-            'h+': this.getHours(), //小时 
-            'm+': this.getMinutes(), //分 
-            's+': this.getSeconds(), //秒 
-            'q+': Math.floor((this.getMonth() + 3) / 3), //季度 
-            'S': this.getMilliseconds() //毫秒 
-        };
-        if (/(y+)/.test(fmt)) {
-            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-        }
-        for (var k in o) {
-            if (new RegExp('(' + k + ')').test(fmt)) {
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
-            }
-        }
-        return fmt;
-    };
     var driverGrowth = echarts.init($('.J_drivegrowth')[0]), //司机增长
         userGrowth = echarts.init($('.J_usergrowth')[0]), //用户增长
         userConsumeGrowth = echarts.init($('.J_userConsumegrowth')[0]), //用户消费增长
@@ -73,11 +53,13 @@ $(function() {
     };
     var getDriveConsumePayGrowthOptions = function(data) {
         var xD = [],
-            yD = [];
+            yD1 = [],
+            yD2 = [];
         for (var item in data) {
             if (data.hasOwnProperty(item)) {
                 xD.push(item);
-                yD.push(data[item]);
+                yD1.push(data[item][0]);
+                yD2.push(data[item][1]);
             }
         }
         var option;
@@ -102,11 +84,11 @@ $(function() {
             series: [{
                 name: '充值',
                 type: 'bar',
-                data: yD
+                data: yD1
             }, {
                 name: '消费',
                 type: 'bar',
-                data: yD
+                data: yD2
             }]
         };
 
@@ -144,7 +126,18 @@ $(function() {
         };
 
     };
-    var getGastationConsumePercentOption = function() {
+    var getGastationConsumePercentOption = function(data) {
+        var xD = [],
+            yD = [];
+        for (var item in data) {
+            if (data.hasOwnProperty(item)) {
+                xD.push(item);
+                yD.push({
+                    value: data[item],
+                    name: item
+                });
+            }
+        }
         var option;
         return option = {
             title: {
@@ -155,61 +148,69 @@ $(function() {
                 formatter: "{a} <br/>{b} : {c} ({d}%)"
             },
             legend: {
-                // orient: 'vertical',
-                // x: 'left',
-                data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '其他']
+                data: xD
             },
             calculable: true,
             series: [{
-                name: '访问来源',
+                name: '加气站消费占比',
                 type: 'pie',
                 radius: '55%',
                 center: ['50%', '60%'],
-                data: [{
-                    value: 335,
-                    name: '直接访问'
-                }, {
-                    value: 310,
-                    name: '邮件营销'
-                }, {
-                    value: 234,
-                    name: '联盟广告'
-                }, {
-                    value: 135,
-                    name: '视频广告'
-                }, {
-                    value: 1548,
-                    name: '搜索引擎'
-                }]
+                data: yD
             }]
         };
 
     }
-    var _simulationData = function() {
-        var now = new Date(),
-            randomNum = function(begin, end) {
-                begin = Number(begin);
-                end = Number(end);
-                var r = Math.random();
-                if (isNaN(begin) || isNaN(end) || begin > end) {
-                    return r;
-                }
-                return Math.floor(r * (end - begin + 1) + begin);
-            },
-            result = {};
-        result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
-        for (i = 1; i < 30; i++) {
-            now.setDate(now.getDate() - 1);
-            result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
-        }
-        return result;
-    };
-    driverGrowth.setOption(getLineChartsOptions('每日司机增长趋势（近一个月）', '司机数', _simulationData()));
-    userGrowth.setOption(getLineChartsOptions('每日用户充值增长趋势（近一个月）', '用户数', _simulationData()));
-    userConsumeGrowth.setOption(getLineChartsOptions('每日用户消费增长趋势（近一个月）', '消费额', _simulationData()));
-    driveConsumePayGrowth.setOption(getDriveConsumePayGrowthOptions(_simulationData()));
-    carOwnerPayGrowth.setOption(getCarOwnerPayGrowthOptions(_simulationData()));
-    gastationConsumeGrowth.setOption(getLineChartsOptions('加气站消费增长趋势（近一个月）', '消费额', _simulationData()));
-    gastationConsumePercent.setOption(getGastationConsumePercentOption('加气站消费增长趋势（近一个月）', '消费额', _simulationData()));
 
+    $.get('assets/js/index.json').then(function(res) {
+        if (res.err_code == 0) {
+            driverGrowth.setOption(getLineChartsOptions('每日司机增长趋势（近一个月）', '司机数', res.driverGrowth));
+            userGrowth.setOption(getLineChartsOptions('每日用户充值增长趋势（近一个月）', '用户数', res.userGrowth));
+            userConsumeGrowth.setOption(getLineChartsOptions('每日用户消费增长趋势（近一个月）', '消费额', res.userConsumeGrowth));
+            driveConsumePayGrowth.setOption(getDriveConsumePayGrowthOptions(res.driveConsumePayGrowth));
+            carOwnerPayGrowth.setOption(getCarOwnerPayGrowthOptions(res.carOwnerPayGrowth));
+            gastationConsumeGrowth.setOption(getLineChartsOptions('加气站消费增长趋势（近一个月）', '消费额', res.gastationConsumeGrowth));
+            gastationConsumePercent.setOption(getGastationConsumePercentOption(res.gastationConsumePercent));
+
+        }
+    });
+    // var _simulationData = function() {
+    //     Date.prototype.format = function(fmt) {
+    //         var o = {
+    //             'M+': this.getMonth() + 1, //月份 
+    //             'd+': this.getDate(), //日 
+    //             'h+': this.getHours(), //小时 
+    //             'm+': this.getMinutes(), //分 
+    //             's+': this.getSeconds(), //秒 
+    //             'q+': Math.floor((this.getMonth() + 3) / 3), //季度 
+    //             'S': this.getMilliseconds() //毫秒 
+    //         };
+    //         if (/(y+)/.test(fmt)) {
+    //             fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    //         }
+    //         for (var k in o) {
+    //             if (new RegExp('(' + k + ')').test(fmt)) {
+    //                 fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+    //             }
+    //         }
+    //         return fmt;
+    //     };
+    //     var now = new Date(),
+    //         randomNum = function(begin, end) {
+    //             begin = Number(begin);
+    //             end = Number(end);
+    //             var r = Math.random();
+    //             if (isNaN(begin) || isNaN(end) || begin > end) {
+    //                 return r;
+    //             }
+    //             return Math.floor(r * (end - begin + 1) + begin);
+    //         },
+    //         result = {};
+    //     result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
+    //     for (i = 1; i < 30; i++) {
+    //         now.setDate(now.getDate() - 1);
+    //         result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
+    //     }
+    //     return result;
+    // };
 });
